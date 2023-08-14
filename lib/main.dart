96141ea4d3235f27f5a90cacb4841852922a5608
YasserBar <yasserbarghooth@gmail.com>
2023-08-14 11:30:35 +0300

    splash & icon logo & color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:nano_tech_cosmetic/core/constants/app_pages_root.dart';
 import 'package:nano_tech_cosmetic/features/auth/data/data_sources/auth_local_data_source.dart';
@@ -17,18 +18,15 @@ void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await di.init();
-  _initializeTokenAndCustomer();
+  initializeTokenAndCustomer();
   runApp(const MyApp());
 }
 
-// void _initializationSplash() async {
-//   await Future.delayed(const Duration(seconds: 3));
-//   FlutterNativeSplash.remove();
-// }
-
-Future<void> _initializeTokenAndCustomer() async {
+void initializeTokenAndCustomer() async {
+  await Future.delayed(const Duration(seconds: 2));
+  FlutterNativeSplash.remove();
   try {
     UserModel userModel = await di.sl<AuthLocalDataSource>().getCachedUser();
     if (kDebugMode) {
